0687d45260a1bc53f230a57e99b1bfad06a71882
LucaFormica17 <145986784+LucaFormica17@users.noreply.github.com>
2023-11-29 16:08:36 +0100

    [UPDATE] solved problem

---
+++ js/script.js
@@ -0,0 +1,20 @@
+function viewGrid(){
+    function createCell(num){
+        const element = document.createElement('div');
+        element.classList.add('square');
+        element.innerText = num;
+        return element;
+    }
+
+    const grid = document.getElementById('grid');
+
+    for(let i=0; i<100; i++){
+        let square = createCell(i+1);
+
+        grid.appendChild(square);
+    }
+}
+let button = document.getElementById('start');
+button.addEventListener('click', function(){
+    viewGrid();
+});
